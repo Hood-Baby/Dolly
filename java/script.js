c6ad4a6b37a4d6fde4332f6cde478a5b0dc5f677
Hood-Baby <127731034+Hood-Baby@users.noreply.github.com>
2025-01-08 23:40:18 +0200

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,9 @@
+/*=========================== typed animation =========================*/
+
+var typing = new Typing(".typing", {
+    strings: ["", "web Designer", "web Developer", "Graphic Designer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  })
+  
